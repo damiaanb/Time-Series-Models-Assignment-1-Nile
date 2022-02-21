0f9a6537ec4fe9a753980e294a33b4115e80a7b6
damiaanb <91317684+damiaanb@users.noreply.github.com>
2022-02-21 22:52:59 +0100

    Code produces also Figure 2.5 now

--- Assignment1_main_general.py
+++ Assignment1_main_general.py
@@ -5,6 +5,17 @@ import math as ms
 import numpy as np
     
 
+def removedata(y, first, last):
+    """
+    Converts y to an array with and for the entry number first to last a value of NaN
+    """
+    y = np.array(y).astype(float)
+    y[first:last] = np.nan 
+    
+    return y
+
+
+
 def inverse(M):
     """
     Take inverse of a matrix M
@@ -13,6 +24,7 @@ def inverse(M):
         res = np.array([[1 / M[0][0]]])
     else:
         res = np.linalg.inv(M)
+        
     return(res)
 
 
@@ -44,15 +56,25 @@ def Kalman_Filter(y, a1, p1, sigma2_eps, sigma2_eta):
     
     for i in range(n):
         
-        
-        v_t[i]  = y[i] - np.dot(Z_t, a[i])
-        F_t[i]  = np.dot(np.dot(Z_t, P[i]), Z_t.T) + H_t
-        K_t[i]  = np.dot(np.dot(np.dot(T_t, P[i]), Z_t.T), inverse(F_t[i]))
-        L_t[i]  = T_t - np.dot(K_t[i], Z_t)
-        q005[i] = np.array([[norm.ppf(0.05, loc = float(a[i]), scale = ms.sqrt(float(P[i])))]])
-        q095[i] = np.array([[norm.ppf(0.95, loc = float(a[i]), scale = ms.sqrt(float(P[i])))]])
-        a[i + 1]   = np.dot(T_t, a[i]) + np.dot(K_t[i], v_t[i])
-        P[i + 1]   = np.dot(np.dot(T_t, P[i]), T_t.T) + np.dot(np.dot(R_t, Q_t), R_t.T) - np.dot(np.dot(K_t[i], F_t[i]), K_t[i].T)
+        if (np.isnan(y[i])):
+            
+            v_t[i]     = np.array([[ms.nan]])
+            F_t[i]     = np.array([[10 ** 7]])
+            K_t[i]     = np.array([[0]])
+            L_t[i]     = T_t - np.dot(K_t[i], Z_t)
+            a[i + 1]   = np.dot(T_t, a[i]) 
+            P[i + 1]   = np.dot(np.dot(T_t, P[i]), T_t.T) + np.dot(np.dot(R_t, Q_t), R_t.T) 
+            
+        else:
+            
+            v_t[i]  = y[i] - np.dot(Z_t, a[i])
+            F_t[i]  = np.dot(np.dot(Z_t, P[i]), Z_t.T) + H_t
+            K_t[i]  = np.dot(np.dot(np.dot(T_t, P[i]), Z_t.T), inverse(F_t[i]))
+            L_t[i]  = T_t - np.dot(K_t[i], Z_t)
+            q005[i] = np.array([[norm.ppf(0.05, loc = float(a[i]), scale = ms.sqrt(float(P[i])))]])
+            q095[i] = np.array([[norm.ppf(0.95, loc = float(a[i]), scale = ms.sqrt(float(P[i])))]])
+            a[i + 1]   = np.dot(T_t, a[i]) + np.dot(K_t[i], v_t[i])
+            P[i + 1]   = np.dot(np.dot(T_t, P[i]), T_t.T) + np.dot(np.dot(R_t, Q_t), R_t.T) - np.dot(np.dot(K_t[i], F_t[i]), K_t[i].T)
 
     return a, P, v_t, F_t, K_t, L_t, q005, q095, n
 
@@ -81,20 +103,26 @@ def  Kalman_Smoother(n, v_t, F_t, L_t, a, P, y, K_t,  sigma2_eps, sigma2_eta):
      
      for j in range(n-1,-1,-1):
          
-         r_t[j - 1]   = np.dot(v_t[j], inverse(F_t[j])) + np.dot(L_t[j], r_t[j])
-         alpha_hat[j] = a[j] + np.dot(P[j], r_t[j - 1])
-          
-         N_t[j - 1]   = np.dot(np.dot(Z_t.T, inverse(F_t[j])), Z_t) + np.dot(np.dot(L_t[j].T, N_t[j]), L_t[j])
-         V_t[j]       = P[j] - np.dot(np.dot(P[j], N_t[j - 1]), P[j])
-         
-         ## For fig 2.3
-         eps_hat[j]   = y[j] - alpha_hat[j]
-         
-         D[j]         = inverse(F_t[j]) + np.dot(np.dot(K_t[j], K_t[j]), N_t[j]) # equation 2.47
-         var_eps_yn[j]= H_t - np.dot(np.dot(H_t, H_t), D[j])
-         
-         eta_hat[j]   = np.dot(Q_t, r_t[j])
-         var_eta_yn[j]= Q_t - np.dot(np.dot(Q_t, Q_t), N_t[j]) # equation 2.47
+         if (np.isnan(y[j])):
+             
+             r_t[j - 1]   =  r_t[j]
+             alpha_hat[j] = a[j] + np.dot(P[j], r_t[j - 1])
+             N_t[j - 1]   = np.dot(np.dot(Z_t.T, inverse(F_t[j])), Z_t) +  N_t[j]
+             V_t[j]       = P[j] - np.dot(np.dot(P[j], N_t[j - 1]), P[j])
+
+         else:
+             
+             r_t[j - 1]   = np.dot(v_t[j], inverse(F_t[j])) + np.dot(L_t[j], r_t[j])
+             alpha_hat[j] = a[j] + np.dot(P[j], r_t[j - 1])
+             N_t[j - 1]   = np.dot(np.dot(Z_t.T, inverse(F_t[j])), Z_t) + np.dot(np.dot(L_t[j].T, N_t[j]), L_t[j])
+             V_t[j]       = P[j] - np.dot(np.dot(P[j], N_t[j - 1]), P[j]) 
+             
+             # For fig 2.3
+             eps_hat[j]   = y[j] - alpha_hat[j]
+             D[j]         = inverse(F_t[j]) + np.dot(np.dot(K_t[j], K_t[j]), N_t[j]) # equation 2.47
+             var_eps_yn[j]= H_t - np.dot(np.dot(H_t, H_t), D[j])
+             eta_hat[j]   = np.dot(Q_t, r_t[j])
+             var_eta_yn[j]= Q_t - np.dot(np.dot(Q_t, Q_t), N_t[j]) # equation 2.47
          
      return r_t, alpha_hat, N_t, V_t, eps_hat, var_eps_yn, eta_hat, var_eta_yn
  
@@ -302,28 +330,102 @@ def Plot23(x, y, eps_hat, var_eps_yn, eta_hat, var_eta_yn, ftsize, lw):
 
 
 
+def Plot25(x, y, a, p, alpha_hat, V_t_mis, ftsize, lw): 
+    """
+    Plots the 4 plots of figure 2.1 of Time Series Analysis by State Space Methods bu Durbin J., Koopman S.J.
+    """
+    
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, constrained_layout = True)
+    fig.set_size_inches(12, 8)
+    
+    # array of 1x1 arrays to a list of values for t = 2, ... 100
+    a         = [float(el) for el in a[1:-1]]
+    p         = [float(el) for el in p[1:]]
+    alpha_hat = [float(el) for el in alpha_hat]
+    V_t_mis   = [float(el) for el in V_t_mis]
+    
+    # SUBPLOT 1 upper left ------------------------------------------------
+    ax1.plot(x, y, color = "black")
+    ax1.plot(x[1:], a, color = "darkslateblue", lw = lw)
+    ax1.tick_params(axis='both', which='major', labelsize=ftsize, width = lw)
+    ax1.set_ylim([450, 1400])
+    for axis in ['bottom','left','right','top']:
+        if axis == 'bottom' or axis == 'left':
+            ax1.spines[axis].set_linewidth(lw)
+        else:
+            ax1.spines[axis].set_visible(False)
+    
+    # SUBPLOT 2 upper right ----------------------------------------------
+    ax2.plot(x, p, color = "darkslateblue", lw=lw)
+    ax2.tick_params(axis='both', which='major', labelsize=ftsize, width = lw)
+    # ax2.set_xlim([1860, 1970])
+    # ax2.set_ylim([5000, 17500])
+    
+    for axis in ['bottom','left','right','top']:
+        if axis == 'bottom' or axis == 'left':
+            ax2.spines[axis].set_linewidth(lw)
+        else:
+            ax2.spines[axis].set_visible(False)
+    
+    # SUBPLOT 3 below left -----------------------------------------------
+    ax3.plot(x, y, color = "black")
+    ax3.plot(x, alpha_hat, color = "darkslateblue")
+    #ax3.plot(x[1:], v, color = "darkslateblue", lw=lw)
+    ax3.tick_params(axis='both', which='major', labelsize=ftsize, width = lw)
+    #ax3.xticks([1880,1900],["akax", "fey"], rotation='vertical')
+    # ax3.set_xlim([1862, 1972])
+    # ax3.set_ylim([-450, 400])
+    #ax3.hlines(0,1860, 1970, color = 'black', lw = lw)
+    for axis in ['bottom','left','right','top']:
+        if axis == 'bottom' or axis == 'left':
+            ax3.spines[axis].set_linewidth(lw)
+        else:
+            ax3.spines[axis].set_visible(False)
+            
+    # SUBPLOT 4 below right ----------------------------------------------
+    ax4.plot(x, V_t_mis, color = "darkslateblue", lw=lw)
+    ax4.tick_params(axis='both', which='major', labelsize=ftsize, width = lw)
+    # ax4.set_xlim([1860, 1970])
+    # ax4.set_ylim([20000, 32500])
+    
+    for axis in ['bottom','left','right','top']:
+        if axis == 'bottom' or axis == 'left':
+            ax4.spines[axis].set_linewidth(lw)
+        else:
+            ax4.spines[axis].set_visible(False)
+
+
 
 def main():
-    data = pd.read_excel("Nile.xlsx", names =["year", "volume"])
-    x = np.array(data["year"])
-    y = np.array(data["volume"])
     
+    # Get Nile data and convert from to DataFrame to Array
+    data       = pd.read_excel("Nile.xlsx", names =["year", "volume"])
+    x          = np.array(data["year"])
+    y          = np.array(data["volume"])
+    y_mis      = removedata(removedata(y,20,40),60,80)
+    
+    # Apply Kalman Filter and Smoother
     a, P, v_t, F_t, K_t, L_t, q005, q095, n                              = Kalman_Filter(y, a1 = 0, p1 = 10 ** 7, sigma2_eps = 15099, sigma2_eta = 1469.1)
     r_t, alpha_hat, N_t, V_t, eps_hat, var_eps_yn, eta_hat, var_eta_yn   = Kalman_Smoother(n, v_t, F_t, L_t, a, P, y, K_t, sigma2_eps = 15099, sigma2_eta = 1469.1)
     
+    # Apply Kalman Filter and Smoother: Missing observation case 
+    a_mis, P_mis, v_t_mis, F_t_mis, K_t_mis, L_t_mis, q005_mis, q095_mis, n                            = Kalman_Filter(y_mis, a1 = 0, p1 = 10 ** 7, sigma2_eps = 15099, sigma2_eta = 1469.1)
+    r_t_mis, alpha_hat_mis, N_t_mis, V_t_mis, eps_hat_mis, var_eps_yn_mis, eta_hat_mis, var_eta_yn_mis = Kalman_Smoother(n, v_t_mis, F_t_mis, L_t_mis, a_mis, P_mis, y_mis, K_t_mis, sigma2_eps = 15099, sigma2_eta = 1469.1)
     
+    # Plot Figures 
     Plot21(x, y, a, P, v_t, F_t, q005, q095, ftsize = 12, lw = 1.5)
     Plot22(x, y, alpha_hat, V_t, r_t, N_t, ftsize = 12, lw = 1.5)
     Plot23(x, y, eps_hat, var_eps_yn, eta_hat, var_eta_yn, ftsize = 12, lw = 1.5)
-    
-    
-    return x, y, a, P, v_t, F_t, K_t, L_t, q005, q095, n, r_t, alpha_hat, V_t, N_t
+    Plot25(x, y_mis, a_mis, P_mis, alpha_hat_mis, V_t_mis, ftsize = 12, lw = 1.5)
 
 
 
 if __name__ == '__main__':
     
-    x, y, a, P, v_t, F_t, K_t, L_t, q005, q095, n, r_t, alpha_hat, V_t, N_t = main()
+     main()
+
+
+
 
 
 
